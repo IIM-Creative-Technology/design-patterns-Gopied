277f0feeef6eef375ecfdedb4cff860416a462b8
Gopied <45171626+Gopied@users.noreply.github.com>
2020-11-18 10:57:36 +0100

    first comm

---
+++ app.js
@@ -0,0 +1,8 @@
+let myVar = 'BONSOIR'
+let myAge = 20
+let myLanguages = ['Français, English']
+
+let myObject = {} 
+let myObject2 = Object.create(object.prototype)
+let myObject3 = new Object()
+
---
+++ exercice6/app.js
@@ -0,0 +1,38 @@
+/switch (e.code){
+    case 'ArrowUp' :
+    case 'ArrowLeft' :
+    case 'ArrowRight' :
+    case 'ArrowDown' :
+        imageManager.execute(e.code)
+        break
+}/
+if (imageManager.hasOwnProperty(e.code)){
+    imageManager.execute(e.code)
+}
+})
+
+let imageManager = {
+image : document.querySelector('.twitter-picture'),
+ArrowUp : function(){
+    this.image.style.top = this.image.offsetTop - 10 + 'px'
+},
+ArrowLeft : function(){
+    this.image.style.left = this.image.offsetLeft - 10 + 'px'
+},
+ArrowRight : function(){
+    this.image.style.left = this.image.offsetLeft + 10 + 'px'
+},
+ArrowDown : function(){
+    this.image.style.top = this.image.offsetTop + 10 + 'px'
+},
+}
+
+imageManager.execute = function (key){
+let methodName = imageManager[key]
+return methodName.apply(imageManager)
+}
+
+setInterval(function(){
+
+}, 500)
+
